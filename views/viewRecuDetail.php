56d67ffbb95794456088f6d9b7a01cdeaca16722
patient-kasongo <20ki180@esisalama.org>
2023-09-05 22:22:54 +0200

    recu

--- views/viewRecuDetail.php
+++ views/viewRecuDetail.php
@@ -4,10 +4,14 @@
     ?>
 
 <div class="container">
-    <p>Numéro : <?= $recu[0]->idRecu ?></p>
-    <p>Somme : <?= $recu[0]->sommeEnChiffre ?> $</p>
-    <p>Somme en toute lettre : <?= $recu[0]->sommeEnLettre ?></p>
-    <p>Appartenant à : <?= $recu[0]->nom.' '.$recu[0]->postnom.' '.$recu[0]->prenom ?></p>
-    <p>Motif : <?= $recu[0]->motif.' '.$recu[0]->mois ?></p>
+    <div class="recu">
+        <h4 class="recu_title">Ekima</h4>
+        <p class="number">Numéro : <?= $recu[0]->idRecu ?></p>
+        <p>Appartenant à : <?= $recu[0]->nom.' '.$recu[0]->postnom.' '.$recu[0]->prenom ?></p>
+        <p>Somme : <?= $recu[0]->sommeEnChiffre ?> $</p>
+        <p>Somme en toute lettre : <?= $recu[0]->sommeEnLettre ?></p>
+        <p>Motif : <?= $recu[0]->motif.' '.$recu[0]->mois ?></p>
+        <p class="signature">Signature(sceau)</p>
+    </div>
     <button class="btn-primary btn" id="print">Imprimer</button>
 </div>
